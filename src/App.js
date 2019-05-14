985577117de52dedbfa738da168d222516031f39
Ubuntu <ubuntu@ip-172-31-42-222.ec2.internal>
2019-05-14 12:00:37 +0000

    Changed text

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
             Edit <code>src/App.js</code> and save to reload.
           </p>
           <p>
-            Health to my family
+            Health to my family - the feature branch
           </p>
           <a
             className="App-link"
